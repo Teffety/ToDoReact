5e87852ca2b85b27b79c700513952c1bf262c67e
Teffety <bubblesjinx@gmail.com>
2020-04-25 23:45:01 +0400

    add

---
+++ precache-manifest.537e6b6245ded89fdd964f057ccc4916.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51a3ad96c4c34d2ec1a754db058d36ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "102b7de2487c3c23e2dc",
+    "url": "/static/css/main.a9880ec5.chunk.css"
+  },
+  {
+    "revision": "85fe7c0aed647b1b6886",
+    "url": "/static/js/2.15d97f6f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.15d97f6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "102b7de2487c3c23e2dc",
+    "url": "/static/js/main.926d2668.chunk.js"
+  },
+  {
+    "revision": "80e45239181130bf717f",
+    "url": "/static/js/runtime-main.43649c24.js"
+  },
+  {
+    "revision": "a82040ecf06139df2f3e653326b3420c",
+    "url": "/static/media/Bangers-Regular.a82040ec.ttf"
+  },
+  {
+    "revision": "e749e21e07ade1074aa76c14d6e0a97b",
+    "url": "/static/media/ComicNeue-Light.e749e21e.ttf"
+  },
+  {
+    "revision": "ed523d48462dd8dfbfd65971aa30bf23",
+    "url": "/static/media/ComicNeue-LightItalic.ed523d48.ttf"
+  },
+  {
+    "revision": "52497ef6e2a57915ff534c342e49a95b",
+    "url": "/static/media/ComicNeue-Regular.52497ef6.ttf"
+  },
+  {
+    "revision": "16dc9819cb371f042475ae8b0c309cba",
+    "url": "/static/media/Poppins-BlackItalic.16dc9819.ttf"
+  },
+  {
+    "revision": "2a47a29ceb33c966c8d79f8d5a5ea448",
+    "url": "/static/media/Poppins-Light.2a47a29c.ttf"
+  },
+  {
+    "revision": "ba95810b56f476990ca71d15139d5111",
+    "url": "/static/media/Poppins-Medium.ba95810b.ttf"
+  }
+]);
